a17dc9d81c3ce7caf1141dca63f5285c85cf53f2
DonaldZheng <dzheng112@gmail.com>
2021-05-05 16:22:30 -0500

    added an isExist function and a buy function in my Cart Controller now I am making the remove function

--- RentalEquipmentCapstone/Controllers/CartController.cs
+++ RentalEquipmentCapstone/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RentalEquipmentCapstone.Helpers;
 using RentalEquipmentCapstone.Models;
+using RentalEquipmentCapstone.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,51 @@ namespace RentalEquipmentCapstone.Controllers
         public IActionResult Index()
         {
             var cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+            ViewBag.cart = cart;
+            ViewBag.total = cart.Sum(item => item.Product.Price * item.Quantity);
+
             return View();
+        }
+        private int isExist(string id)
+        {
+            List<Item> cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+            for (int i = 0; i < cart.Count; i++)
+            {
+                if (cart[i].Product.Id.Equals(id))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public IActionResult Buy(string id)
+        {
+            ProductViewModel productModel = new ProductViewModel();
+            if (SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart") == null)
+            {
+                List<Item> cart = new List<Item>();
+                cart.Add(new Item { Product = productModel.find(id), Quantity = 1 });
+                SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
+            }
+            else
+            {
+                List<Item> cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+                int index = isExist(id);
+                if (index != -1)
+                {
+                    cart[index].Quantity++;
+                }
+                else
+                {
+                    cart.Add(new Item { Product = productModel.find(id), Quantity = 1 });
+                }
+                SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Remove(string id)
+        {
+        
         }
     }
 }
