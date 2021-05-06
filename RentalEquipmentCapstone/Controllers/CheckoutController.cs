2eeb2d6477325a228e0fb0260c1b5b89d1fa6e88
DonaldZheng <dzheng112@gmail.com>
2021-05-05 19:49:44 -0500

    added images to my checkout page

--- RentalEquipmentCapstone/Controllers/CheckoutController.cs
+++ RentalEquipmentCapstone/Controllers/CheckoutController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using RentalEquipmentCapstone.Helpers;
 using RentalEquipmentCapstone.Models;
+using Stripe;
 
 namespace RentalEquipmentCapstone.Controllers
 {
@@ -15,6 +16,43 @@ namespace RentalEquipmentCapstone.Controllers
             var cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
             ViewBag.cart = cart;
             ViewBag.total = cart.Sum(item => item.Product.Price * item.Quantity);
+            return View();
+        }
+        public IActionResult Charge(string stripeEmail, string stripeToken)
+        {
+            var customers = new CustomerService();
+            var charges = new ChargeService();
+
+            var customer = customers.Create(new CustomerCreateOptions
+            {
+                Email = stripeEmail,
+                Source = stripeToken
+            });
+
+            var charge = charges.Create(new ChargeCreateOptions
+            {
+                Amount = 50000,
+                Description = "AMOUNT DUE",
+                Currency = "USD",
+                Customer = customer.Id,
+                ReceiptEmail = stripeEmail,
+                Metadata = new Dictionary<string, string>()
+                {
+                    {"OrderId", "01" },
+                    {"PostCode", "53202" }
+                }
+
+
+            });
+            if (charge.Status == "succeeded")
+            {
+                string BalanceTransactionId = charge.BalanceTransactionId; // if successful give balance transaction #
+                return View();
+            }
+            else
+            {
+
+            }
             return View();
         }
     }
