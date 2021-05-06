e356c0899a8e866e495fa80d025e8987896556ab
DonaldZheng <dzheng112@gmail.com>
2021-05-05 20:08:09 -0500

    move some html in the customer index for admin and customer view

--- RentalEquipmentCapstone/Migrations/20210506005056_Initial.Designer.cs
+++ RentalEquipmentCapstone/Migrations/20210506005056_Initial.Designer.cs
@@ -10,7 +10,7 @@ using RentalEquipmentCapstone.Data;
 namespace RentalEquipmentCapstone.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20210505195024_Initial")]
+    [Migration("20210506005056_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
